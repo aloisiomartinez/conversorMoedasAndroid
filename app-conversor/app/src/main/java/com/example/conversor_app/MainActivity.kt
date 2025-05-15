d6b344331dd58896e9db6161899b41b6fd42b6d2
aloisiomartinez <aloisiojacobs36@gmail.com>
2025-05-15 16:15:25 -0300

    spinner adapter

--- app-conversor/app/src/main/java/com/example/conversor_app/MainActivity.kt
+++ app-conversor/app/src/main/java/com/example/conversor_app/MainActivity.kt
@@ -9,17 +9,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
-import com.example.conversor_app.network.KtorHttpClient
+import com.example.conversor_app.databinding.ActivityMainBinding
+import com.example.conversor_app.ui.CurrencyTypesAdapter
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private val viewModel by viewModels<CurrencyExchangeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,23 +33,24 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.apply {
             launch {
                 viewModel.currencyTypes.collect { result ->
-                    result.onSuccess {
-                        Toast.makeText(this@MainActivity, it.size.toString(), Toast.LENGTH_SHORT).show()
+                    result.onSuccess { currencyTypes ->
+                        binding.spnFromExchange.adapter = CurrencyTypesAdapter(currencyTypes)
+                        binding.spnToExchange.adapter = CurrencyTypesAdapter(currencyTypes)
                     }.onFailure {
-                        Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@MainActivity, it.message,
+                            Toast.LENGTH_LONG).show()
                     }
                 }
             }
             launch {
-                viewModel.exchangeRate.collect {
-                    it.onSuccess {
-                        Log.d("ResultSuccess", "Result Successs $it")
+                viewModel.exchangeRate.collect { result ->
+                    result.onSuccess {
+                        Log.d("MainActivity", it.toString())
                     }.onFailure {
-                        Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
+                        Log.d("MainActivity", it.message.toString())
                     }
                 }
             }
         }
     }
-
 }
---
+++ app-conversor/app/src/main/java/com/example/conversor_app/ui/CurrencyTypesAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.conversor_app.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.example.conversor_app.R
+import com.example.conversor_app.databinding.ItemCurrencyTypeBinding
+import com.example.conversor_app.network.model.CurrencyType
+
+
+class CurrencyTypesAdapter(
+    private val currencyTypes: List<CurrencyType>
+) : BaseAdapter() {
+
+    override fun getCount(): Int {
+        return currencyTypes.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return currencyTypes[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return currencyTypes[position].hashCode().toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        return convertView ?: run {
+            val item = currencyTypes[position]
+            val binding = ItemCurrencyTypeBinding.inflate(LayoutInflater.from(parent?.context))
+            with(binding) {
+                tvCurrencyAcronym.text = item.acronym
+                ivFlag.setImageResource(R.drawable.img_flag_br)
+            }
+            binding.root
+        }
+    }
+}
+
